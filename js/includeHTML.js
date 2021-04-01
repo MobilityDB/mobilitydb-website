3de8c4c2c8c8d0bd0f4a5e24ba12f030fed33698
Robin <rchoquet@student.42.fr>
2021-04-01 15:46:49 +0200

    Improve Navbar

---
+++ js/includeHTML.js
@@ -0,0 +1,57 @@
+// Include html content
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
+        }
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function smallNav() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+//hover current tab in navbar
+var path = window.location.pathname;
+var page = path.split("/").pop();
+var pagename = page.split(".")[0];
+var idname = "nav".concat(pagename);
+var tab = document.getElementById(idname);
+var tab = document.getElementById("navBar");
+console.log(tab);
+console.log(idname);
+tab.classList.add('w3-white');
+//
+function navremove() {
+    tab.classList.remove('w3-white');
+}
+
+this.onunload(navremove())
+
+
